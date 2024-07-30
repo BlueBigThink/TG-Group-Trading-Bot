1089cb6666a9711f0edd4f21f4539d8b3210cffa
bluebigthink <bluebigthink@gmail.com>
2024-07-31 00:48:54 +0200

    bot basic functions

--- main_bot.py
+++ main_bot.py
@@ -5,14 +5,40 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_telegram_bot.settings')
 django.setup()
 from asgiref.sync import sync_to_async
 import logging
-from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext, ApplicationBuilder
+from telegram import (
+    Update, 
+    InlineKeyboardButton,
+    ReplyKeyboardRemove,
+    CallbackQuery,
+    KeyboardButton,
+    InlineKeyboardMarkup,
+    ReplyKeyboardMarkup
+)
+from telegram.ext import (
+    Updater,
+    ContextTypes,
+    CommandHandler, 
+    CallbackContext, 
+    ApplicationBuilder,
+    ConversationHandler,
+    CallbackQueryHandler,
+    MessageHandler,
+    filters
+)
 
 from tg_bot_app.views import MnemonicManager, UserManager
 from dotenv import load_dotenv
 
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 mnemonicManager = MnemonicManager()
 userManager = UserManager()
+
+MAIN = range(1)
 
 async def start(update: Update, context: CallbackContext) -> None:
     user = update.effective_user
@@ -28,34 +54,78 @@ async def start(update: Update, context: CallbackContext) -> None:
         return
 
     await sync_to_async(userManager.init)(user_id, user_name, real_name)
+    keyboard = [
+        [
+            KeyboardButton("Profile"),
+        ]
+    ]
+    # reply_inline_markup = InlineKeyboardMarkup(keyboard)
+    reply_keyboard_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True, one_time_keyboard=True)
+    await update.message.reply_text(
+        f'Welcome {user_name}! ',
+        # reply_markup=reply_inline_markup
+        reply_markup=reply_keyboard_markup
+    )
+    return MAIN
+########################################################################
+#                              +Deposit                                #
+########################################################################
+async def _func_profile(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    query = update.callback_query
+    userInfo = update.message.from_user
+    user_name = userInfo['username']
+    msg = f"{user_name}'s profile"
+    keyboard = [
+        [
+            InlineKeyboardButton("Cancel", callback_data="Cancel"),
+        ]
+    ]
+    await query.message.edit_text(
+        msg,
+        reply_markup=InlineKeyboardMarkup(keyboard)
+    )
 
-    await update.message.reply_text(f'Welcome {user_name}!')
+########################################################################
+#                        +Message Handler                              #
+########################################################################
+async def _handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:    
+    text = update.message.text
 
-    # try:
-    #     user_data = await sync_to_async(User.objects.get)(user_id=user_id)
-    #     await update.message.reply_text(f'Welcome back {user_data.first_name}!')
-    # except User.DoesNotExist:
-    #     new_user = User(
-    #         user_id=user_id,
-    #         first_name=update.effective_user.first_name,
-    #         last_name=update.effective_user.last_name,
-    #         username=update.effective_user.username
-    #     )
-    #     await sync_to_async(new_user.save)()
-    #     logging.info(f'New user {new_user.first_name} has been added to the database')
-    #     await update.message.reply_text('Welcome to django-telegram-bot!')
+    if text == 'Profile':
+        keyboard = [
+            [
+                InlineKeyboardButton("OK", callback_data="OK"),
+            ]
+        ]
+        await update.message.reply_text(
+            "Your profile",
+            reply_markup=InlineKeyboardMarkup(keyboard)
+        )
+
+async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Bye! I hope we can talk again some day.", reply_markup=ReplyKeyboardRemove()
+    )
+    return ConversationHandler.END
 
 def main() -> None:
     mnemonicManager.init()
-    logging.basicConfig(
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        level=logging.INFO
-    )
-    logger = logging.getLogger(__name__)
     load_dotenv()
     token = os.getenv('BOT_TOKEN')
     application = ApplicationBuilder().token(token).build()
-    application.add_handler(CommandHandler('start', start))
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler("start", start)],
+        states={
+            MAIN: [ CallbackQueryHandler(_func_profile, pattern="Profile"),
+                    MessageHandler(filters.TEXT, _handle_message)],            
+        },
+        fallbacks=[CommandHandler("end", end)],
+        allow_reentry=True,
+    )
+    application.add_handler(conv_handler)
     application.run_polling()
 
 if __name__ == '__main__':
